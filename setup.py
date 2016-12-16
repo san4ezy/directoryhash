8dcdf972567cac2ba6bbdee1fec6fb874fa856a3
alexander <san4ezy@gmail.com>
2016-12-16 10:53:51 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="<package_name>",
-    version=__import__('<package_name>').__version__,
+    name="directoryhash",
+    version=__import__('directoryhash').__version__,
     description=open(os.path.join(os.path.dirname(__file__), "DESCRIPTION")).read(),
     license="The MIT License (MIT)",
-    keywords="<keywords>",
+    keywords="directoryhash, directory_hash, hash of directory, directory md5, directory sha1, folder hash, "
+             "md5 folder, sha1 folder",
 
-    author="<name>",
-    author_email="<email>",
+    author="Alexander Yudkin",
+    author_email="alexander@yudkin.com.ua",
 
-    maintainer="<name>",
-    maintainer_email="<email>",
+    maintainer="Alexander Yudkin",
+    maintainer_email="alexander@yudkin.com.ua",
 
-    url="<site_url>",
+    url="yudkin.com.ua",
     packages=find_packages(exclude=["tests.*", "tests"]),
     classifiers=[
         'Intended Audience :: Developers',
-        'Framework :: Django',
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
